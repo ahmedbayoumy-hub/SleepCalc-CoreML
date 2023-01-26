bf31f0eb023f600eac06eb11ba196930d3d4a28c
ahmedbayoumy-hub <abayoum000@gmail.com>
2023-01-26 13:18:27 -0500

    Initial Commit

---
+++ SleepCalculator/SleepCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepCalculatorApp.swift
+//  SleepCalculator
+//
+//  Created by Consultant on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SleepCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
